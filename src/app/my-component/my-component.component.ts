e33d96c73a1cad988ec665dc37a4ed1c5baee226
Mathieu Le Han <mathieu.lehan@gmail.com>
2019-03-13 17:13:39 +0100

    Question 6 (ajout du select) & 7

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MyComponentComponent implements OnInit {
 
   id: string = '';
+  pokemonChoisi: string = '';
   pokemons = ['Pikachu', 'Salamèche', 'Carapuce', 'Bulbizarre'];
 
   constructor() { }
