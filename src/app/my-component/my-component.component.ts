8d6b58b6123aa477b80e4ee0f8d82a44081fdec2
Mathieu Le Han <mathieu.lehan@gmail.com>
2019-03-13 17:04:16 +0100

    Question 5 & 6

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MyComponentComponent implements OnInit {
 
   id: string = '';
+  pokemons = ['Pikachu', 'Salamèche', 'Carapuce', 'Bulbizarre'];
 
   constructor() { }
 
